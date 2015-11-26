184c555dbcaa48d992d5ab60f6271065cda7e903
Andrew Kinley <adkinley@gmail.com>
2015-11-26 06:18:42 -0500

    Added angular-chart.js plus tested functionality of chart

--- client/app/app.js
+++ client/app/app.js
@@ -6,7 +6,8 @@ angular.module('kinleyStockmarketApp', [
   'ngSanitize',
   'ngRoute',
   'btford.socket-io',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'chart.js'
 ])
   .config(function ($routeProvider, $locationProvider, $httpProvider) {
     $routeProvider
